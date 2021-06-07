ad7a8698337c577b45770484fb23b17fc991666c
ecohnoch <542305306@qq.com>
2021-06-07 10:08:39 +0800

    with redis

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 import flask
-
+import redis
 
 app = flask.Flask(__name__)
 
 @app.route('/')
 def hello():
     return "Hello World"
+
+@app.route('/set')
+def set():
+    key = flask.request.args.get('key')
+    value = flask.request.args.get('value')
+    r = redis.Redis(host='10.109.93.175', port=6370)
+    r.set(name=key, value=value)
+    return "OK"
+
+@app.route('/get')
+def get():
+    key = flask.request.args.get('key')
+    r = redis.Redis(host='10.109.93.175', port=6370)
+    value = r.get(key)
+    return value
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
